34e29cd50057f65de63eeb639ee61e26f584c897
Julio Coelho <julio.coelho@avenuesec.io>
2020-09-09 14:55:57 -0300

    #4 - Handling Errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
 
-if [ event_type != "closed" ]; then
+if [ $event_type != "closed" ]; then
     echo "::debug:: The event type was '$event_type'"
     exit 0
 fi
@@ -23,8 +23,13 @@ release_url=$(dotnet gitreleasemanager create \
 --milestone $milestone_name \
 --targetcommitish $GITHUB_SHA \
 --token $repo_token \
---owner $owner
+--owner $owner \
 --repository $repository)
+
+if [ $? -ne 0 ]; then
+    echo "::error::Failed to create the release draft"
+    exit 1
+fi
 
 echo "::set-output name=release-url::$release_url"
 
